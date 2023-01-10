d6c1c46bb9059876bd2f34d0ed5dcd2a1e26885a
504 <504@DESKTOP-DBOC81R>
2023-01-10 17:57:05 +0900

    DAY08

---
+++ Frontend/public/js/0.과제/과제1.js
@@ -0,0 +1,59 @@
+/*console.log('JS실행') //확인용*/
+
+/*------------------------------------과제1------------------------------------*/
+
+// 4. 배열 선언
+	// - 함수 안에서 선언 ---> 함수 실행마다 선언
+	// - 함수 밖에서 선언 ---> JS 실행시 한 번 선언
+// 4. 배열 선언과 동시에 3개의 요소 저장
+let studentArray = ['20230110', '20230109', '20230108']
+
+// 3. HTML : onLogin()에 대한 함수 정의/만들기
+function onLogin(){
+	
+	//console.log('onLogin함수실행') //확인용
+	
+	//5. <input> 마크업 JS변수로 가져오기
+	let sno = document.querySelector('.sno')
+	//5. <input> 마크업에 입력된 데이터 호출
+	let snoValue = sno.value;
+	console.log('5번 체크 : ' + snoValue) //5번 확인용
+	
+	//6. 찾기 [ 만약에 배열에 입력한 값이 존재하면 찾은 인덱스 / 존재하지 않으면 -1 표시 ]
+	let sIndex = studentArray.indexOf(snoValue)
+	console.log('6번 체크: ' + sIndex) //6번 확인용
+	
+	//7. 논리
+	if(sIndex==-1){
+		alert('로그인 실패')
+		document.querySelector('.resultbox').innnerHTML = '로그인 실패'
+	}
+	else{
+		alert('로그인 성공');
+		document.querySelector('.resultbox').innnerHTML = '로그인 성공'
+	}	
+}
+
+/*------------------------------------과제2------------------------------------*/
+console.log('실행') //확인용
+studentArray = push(index(0, 1, 2));
+function onBtn(){
+		
+	let num = document.querySelector('.num')
+	let numValue = num.value;
+	console.log('5번 체크 : ' + numValue) //5번 확인용
+	
+	let xIndex = studentArray.push(numValue)
+	console.log('6번 체크: ' + xIndex) //6번 확인용
+	
+	if(xIndex==0){
+		alert('등록')
+		document.querySelector('.resultbox').innnerHTML = '등록'
+	}
+	else{
+		alert('등록');
+		document.querySelector('.resultbox').innnerHTML = '등록'
+	}	
+}
+
+/*------------------------------------과제3------------------------------------*/
