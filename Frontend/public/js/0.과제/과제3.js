f527b1d6205c37564da5e2aa5faec3f1a619db89
504 <504@DESKTOP-DBOC81R>
2023-01-11 17:26:54 +0900

    .

---
+++ Frontend/public/js/0.과제/과제3.js
@@ -0,0 +1,20 @@
+/*console.log('실행')//확인용
+*/
+let 테이블행 = '<tr> <th>번호</th> <th>방문록</th> </tr>';
+let contentArray = ['']
+
+function onResult(){
+	console.log('클릭함')//확인용
+	
+	let txt_box = document.querySelector('.txt_box').value
+	
+	document.querySelector('.box_table').innerHTML = 테이블행
+}
+
+
+
+
+
+
+
+ 
