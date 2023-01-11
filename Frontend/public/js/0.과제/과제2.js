cfc9b6b2b53a73eaaab7a3ecf0ce62b88d441a6e
504 <504@DESKTOP-DBOC81R>
2023-01-11 16:46:19 +0900

    .

--- Frontend/public/js/0.과제/과제2.js
+++ Frontend/public/js/0.과제/과제2.js
@@ -1,7 +1,9 @@
 console.log
 ('------------과제2-----------')
+
 // 밖에 만들면 결과가 누적됨
 //let 테이블행 = '<tr> <th>단</th> <th>곱</th> <th>결과</th> </tr>'; 
+
 function onResult(){
 	console.log('클릭함')//확인용
 	
@@ -12,7 +14,6 @@ function onResult(){
 	let gab = document.querySelector('.gab').value
 	
 	for( let 곱 = 1 ; 곱<=gab ; 곱++ ){ //for S
-		
 		테이블행 += '<tr> <th>' + dan + '</th> <th>' + 곱 + '</th> <th>' + (dan*곱) + '</th> </tr>';
 	} //for E
 	document.querySelector('.gu_table').innerHTML = 테이블행
