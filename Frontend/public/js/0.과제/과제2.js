58630866e8e8d8366fbef08e8b68ee19dcce2cf9
504 <504@DESKTOP-DBOC81R>
2023-01-11 16:45:47 +0900

    .

--- Frontend/public/js/0.과제/과제2.js
+++ Frontend/public/js/0.과제/과제2.js
@@ -1,11 +1,20 @@
-console.log('------------과제2-----------')
-// 3. 행 반복문으로 만들기
-let 테이블행 = '<tr> <th>단</th> <th>곱</th> <th>결과</th> </tr>'; 
-
-for( let 곱 = 1 ; 1<=9 ; 곱++ ){
-	console.log(곱)//확인용
-
-	테이블행 += '<tr> <th>2</th> <th>' + 곱 + '</th> ' + <th>2*곱</th> + '</tr>';
-
+console.log
+('------------과제2-----------')
+// 밖에 만들면 결과가 누적됨
+//let 테이블행 = '<tr> <th>단</th> <th>곱</th> <th>결과</th> </tr>'; 
+function onResult(){
+	console.log('클릭함')//확인용
+	
+	// 클릭할 때마다 실행되기에 누적되지 않음
+	let 테이블행 = '<tr> <th>단</th> <th>곱</th> <th>결과</th> </tr>'; 
+	
+	let dan = document.querySelector('.dan').value
+	let gab = document.querySelector('.gab').value
+	
+	for( let 곱 = 1 ; 곱<=gab ; 곱++ ){ //for S
+		
+		테이블행 += '<tr> <th>' + dan + '</th> <th>' + 곱 + '</th> <th>' + (dan*곱) + '</th> </tr>';
+	} //for E
+	document.querySelector('.gu_table').innerHTML = 테이블행
 }
-document.querySelector('.gu_table').innerHTML = 테이블행
+
