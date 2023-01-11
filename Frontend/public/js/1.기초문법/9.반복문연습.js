a65896022c08e70e6128144f6862de9b1d0b79c9
504 <504@DESKTOP-DBOC81R>
2023-01-11 14:50:44 +0900

    .

--- Frontend/public/js/1.기초문법/9.반복문연습.js
+++ Frontend/public/js/1.기초문법/9.반복문연습.js
@@ -23,14 +23,20 @@ document.querySelector('body').innerHTML = output
 // 예4) 누적합계 : i는 1부터 10이하까지 1씩 증가 반복
 console.log('---------예4--------')
 let sum = 0 ;
-for( let i = 1; 1<=10; i++ ){ sum += 1 }
+for( let i = 1; 1<=10; i++ ){ 
+	console.log( "i = " + i + "\t"+i+"<=10 " + (i<=10 ) +"\t"+ sum+"+="+i +"\t i++" ) 
+	sum += 1 
+}
 console.log( sum )
 
 // 예5) 누적합계 : i는 1부터 100이하까지 7배수 누적합계
 console.log('---------예5--------')
 // 첫 번째 방법 : [i+=7] 
 sum = ''
-for( let i = 0 ; 1<=100; i+=7 ){ sum += 1 }
+for( let i = 0 ; 1<=100; i+=7 ){ 
+	console.log( "i = " + i + "\t"+i+"<=100 " + (i<=100 ) +"\t"+ sum+"+="+i +"\t i++" ) 
+	sum += 1 
+}	
 console.log( sum )
 
 // 두 번째 방법 : [i % 7 == 0]
@@ -40,5 +46,92 @@ for( let i = 1; 1<=100; i++ ){ // for S
 	} // if E
 } // for E
 console.log( sum )
+
+// 예6) 구구단(2단) [ 단 :2 / 곱 : 변수 ]
+console.log('---------예6--------')
+output = '' // 깡통
+for( let 곱 = 1 ; 곱<=9; 곱++ ){ //for S
+	// 곱은 1부터 9까지 1씩 증가하면서 반복한다
+	console.log( ' 2 * ' + 곱 + ' = ' + ( 2*곱 ) );
+				// '문자' + 변수 + '문자' + (계산식)
+	output += ' 2 * ' + 곱 + ' = ' + ( 2*곱 ) + '<br>'
+} //for E
+document.querySelector('body').innerHTML = output
+
+// 예7) 구구단 [ 단 : 2~9 / 곱 : 1~9(변수) ]
+/* 
+// 7-1. 단 만들기
+for( let 단 = 2 ; 곱<=9 ; 단++ ){
+	console.log( 단 )
+}
+// 7-2. 곱 만들기
+for( let 곱 = 1 ; 곱<=9 ; 곱++ ){
+	console.log( 곱 )
+}
+// 7-3. 단, 곱 합치기 ---> '단'이 한 바퀴 돌 때, '곱'은 아홉 바퀴
+for( let 단 = 2 ; 곱<=9 ; 단++ ){
+	console.log( 단 )
+	for( let 곱 = 1 ; 곱<=9 ; 곱++ ){
+	console.log( 곱 )
+	}
+}
+*/
+// 7-4. 출력
+for( let 단 = 2 ; 곱<=9 ; 단++ ){ //for1 S
+	/*console.log( 단 )*/
+	for( let 곱 = 1 ; 곱<=9 ; 곱++ ){ //for2 S
+	console.log( 단 + ' * ' + 곱 + ' = ' + ( 단*곱 ) )
+	} //for2 E
+} //for1 E
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
 
 
