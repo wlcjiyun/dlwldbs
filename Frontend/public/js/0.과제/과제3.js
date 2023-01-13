60a27b1d20ca989f25b8b89e67fcdd0518211213
504 <504@DESKTOP-DBOC81R>
2023-01-13 15:14:19 +0900

    .

--- Frontend/public/js/0.과제/과제3.js
+++ Frontend/public/js/0.과제/과제3.js
@@ -1,28 +1,32 @@
 /*console.log( '1.js작동')*/
 
-let contentArray = [ ] // 누적 기록 
+let contentArray = [ ] // 누적
+
 // 1. 등록 버튼 클릭 했을때 이벤트 함수
 function addContent(){ // f s
-	//function : 함수 선언[정의]시 사용되는 키워드
-		// addContent : 함수[아무거나]
-			// ( ) : 인수
-				// { } : 함수 호출시 실행되는 구역
+//function : 함수 선언[정의]시 사용되는 키워드
+// addContent : 함수[아무거나]
+// ( ) : 인수
+// { } : 함수 호출시 실행되는 구역
+
 	/*console.log('1.addContent() 함수 작동')*/
 	
-	// 2. <input> 에서 입력받은 데이터[value] 가져와서 변수에 저장 
+	// 2. <input> 에서 입력받은 데이터 가져와서 변수에 저장 
 	let content = document.querySelector('.content').value
 		/*console.log( '2. content : ' + content)*/
+		
 	// 3. 배열내 입력받은 변수 저장 
 	contentArray.push( content )
 		/*console.log( '3. contentArray : ' + contentArray)*/
 	
-	// 4. 출력할 html 구성 [ + vs ` ]
+	// 4. 출력할 html 
 	let html = `<tr><th>번호</th><th>방문내용</th></tr>`
 	
 	// 5. 내용추가 [ 반복 이용한 배열내 요소 출력 ]
 	for( let i = 0 ; i<contentArray.length; i++ ){
 		html += `<tr><td>${ i+1 }</td><td>${ contentArray[i] }</td></tr>`	
 	}
+	
 	// 6. 위에서 구성된 html를 해당 마크업에 대입
 	document.querySelector('.contentTable').innerHTML = html
 	
--- Frontend/public/js/0.과제/과제3추가.js
+++ Frontend/public/js/0.과제/과제3추가.js
@@ -1,34 +1,40 @@
-console.log( '1.js작동')
+/*console.log( '1.js작동')*/
 
 let contentArray = [ ] // 누적 기록 
+
 // 1. 등록 버튼 클릭 했을때 이벤트 함수
-function addContent(){ // f s
+function addContent(){ 
 	let content = document.querySelector('.content').value // 2. <input> 에서 입력받은 데이터[value] 가져와서 변수에 저장 
 	contentArray.push( content ) // 3. 배열내 입력받은 변수 저장 
 	printContent( )
-} // f e
+} 
+
 // 2. 삭제 버튼을 클릭했을때 함수 
-function onDelete( dno ){ // f s 
+function onDelete( dno ){ 
 	contentArray.splice( dno , 1 )
 	printContent( )
-} // f e 
+} 
+
 // 3. 배열내 존재하는 요소들을 출력하는 함수 [ 1. 등록했을때 2.삭제했을때 ]
 function printContent( ){
-	// 4. 출력할 html 구성 [ + vs ` ]
+	// 4. 출력할 html 구성
 	let html = `<tr>			
 					<th>번호</th>
 					<th>방문내용</th>
 					<th>비고</th>
-				</tr>` 			
-	for( let i = 0 ; i<contentArray.length; i++ ){ // 5. 내용추가 [ 반복 이용한 배열내 요소 출력 ]
+				</tr>` 
+				
+	 // 5. 내용추가 [ 반복 이용한 배열내 요소 출력 ]						
+	for( let i = 0 ; i<contentArray.length; i++ ){
 		html += `<tr>
 					<td>${ i+1 }</td>
 					<td>${ contentArray[i] }</td>
 					<td><button onclick="onDelete( ${i} )">삭제</button></td>
 				</tr>`	
-												/* onDelete( 삭제할번호 ) */
-	} // for end 
-	document.querySelector('.contentTable').innerHTML = html // 6. 위에서 구성된 html를 해당 마크업에 대입
+												
+	} 
+	// 6. 위에서 구성된 html를 해당 마크업에 대입
+	document.querySelector('.contentTable').innerHTML = html 
 }
 
 
