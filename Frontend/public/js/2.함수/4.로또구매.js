c6fc6909bc02295d8691ae159225b942c0bfdc4e
504 <504@DESKTOP-DBOC81R>
2023-01-13 12:18:17 +0900

    .

--- Frontend/public/js/2.함수/4.로또구매.js
+++ Frontend/public/js/2.함수/4.로또구매.js
@@ -16,9 +16,9 @@ function 버튼출력(){ // F S
 	// 출력과 동시에 취소 버튼 활성화
 	document.querySelector('.취소버튼').style.display = 'inline'
 	
-}// F E
+} // F E
 
-//2. 숫자 버튼을 없애는 함수
+// 2. 숫자 버튼을 없애는 함수
 function 버튼출력취소(){
 	// 해당 div에 공백으로 변경하다
 	document.querySelector('.버튼목록상자').innerHTML = ''
@@ -26,29 +26,45 @@ function 버튼출력취소(){
 	document.querySelector('취소버튼').style.display = 'none'
 }
 
-//1. 선택한 숫자를 6개 저장하는 배열
+// 1. 선택한 숫자를 6개 저장하는 배열
 let 선택번호목록 = [ ]
 
-//3. 버튼 클릭 함수
-function 버튼클릭( i ){ // F S
-	console.log( i + '번호를 선택하셨네요!' )
+// 3. 버튼 클릭 함수
+function 버튼클릭( 번호 ){ // F S
+	console.log( 번호 + '번호를 선택하셨네요!' )
 	
 	// 1. 중복검사
-	if( 선택번호목록.indexOf(i) >= 0 ){
-		alert('이미 선택한 번호입니다')
-		return; // 반환값은 없지만 함수를 끝낼 수 있다[실행종료]
+	if( 선택번호목록.indexOf(번호) >= 0 ){
+		alert('이미 선택한 번호입니다[해당 버튼 취소됩니다]')
+		선택번호목록.splice( 선택번호목록.indexOf(번호), 1 )
+		추첨버튼출력()
+		return;
 	}
 	// 2. 6개까지만 저장
 	if(선택번호목록.length == 6 ){
 		alert('이미 모두(6개)를 선택했습니다')
 		return;
 	}
-	//2가지 유효성 검사를 충족하지 않았을 때 저장
-	선택번호목록.push( i )
-	document.querySelector('.선택번호상자').innerHTML = 선택번호목록
-	
-		
+	//2가지 유효성 검사를 충족하지 않았을 때 저장		
+	선택번호목록.push( 번호 )
+	추첨버튼출력();
+}	
+
+// 4. 선택번호목록 출력 및 추첨 버튼 활성화
+function 추첨버튼출력( ){
+	//추첨버튼 활성화/비활성화
+	if( 선택번호목록.length == 6 ){
+		document.querySelector('.추첨버튼').style.display = 'inline'
+	}else{
+		document.querySelector('.추첨버튼').style.display = 'none'
+	}
+	document.querySelector('.선택번호상자').innerHTML = 선택번호목록	
 } // F E
+
+
+
+
+
 
 
 
@@ -58,4 +74,3 @@ function 버튼클릭( i ){ // F S
 // 2. 백틱 이용한 문자열처리
 	// 1. 전체를 `으로 감싼다
 	// 2. 변수/수식는 ${변수/수식} 감싼다 
-
