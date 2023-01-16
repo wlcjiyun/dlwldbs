f07022711997d1051aabfd3709dfcf34bc64552d
504 <504@DESKTOP-DBOC81R>
2023-01-16 11:41:01 +0900

    .

--- Frontend/public/js/2.함수/5.틱택토.js
+++ Frontend/public/js/2.함수/5.틱택토.js
@@ -1,20 +1,29 @@
-//alert('실행')
+//alert('1.js실행')
 
+// 4.배열선언 [ 버튼의 상태를 저장하는 배열 -> null : 빈자리 ]
+let 버튼목록현황 = [ null , null , null , null , null , null ,null , null , null ]
 // 1. 함수 정의
 function 클릭이벤트( 버튼번호 ){
-// funcition : 함수를 정의할 때 사용되는 키워드
-// 클릭이벤트	 : 함수명[아무거나]
-// ( )		 : 인수		: 해당 함수를 호출할 때 함수로 들어오는 값/데이터
-// return  	 : 반환값		: 해당 함수가 종료될 때 호출했던 곳으로 보내는 값/데이터
-	alert( '클릭함수 실행 : ' + 버튼번호 )
+	// 1. 선택된 번호와 알모양을 알두기 함수 전달하고 반환이 false이면 알두기 실패 / true 성공 후 컴퓨터 차례
+		//  ! : 반대/부정 false->true   / true->false
+	if( ! 알두기( 버튼번호 , 'O' ) ) { return; }
 	
-	// 2. <div> 버튼 목록 가져와서 배열에 저장 [ div 9개 호출 ]
-	let 버튼목록 = document.querySelectorAll( '.버튼' ) 
-	console.log( 버튼목록 )
+	// 2. 컴퓨터 차례 무한루프 [ 정상적으로 알 두기 전까지 ]
+	while( true ){
+		let 컴퓨터번호 = parseInt( Math.random()*9 )+1 // 1~9 사이의 난수 생성
+		if( 알두기( 컴퓨터번호 , 'X' ) ) { return; }
+	}
 	
-	// 3. 클릭된 번호의 div 찾기 [ 선택번호-1 ]
-			// -1 : 인덱스는 0부터 시작하기에
-	버튼목록[ 버튼번호-1 ].innerHTML = 'O';
-	
-	return;
+}
+
+// 2. 알두기 [ 반환값 : 알 정상적으로 두었는지 확인 false:0:'실패'  / true:1:'성공' -> 신호 번호 ]
+function 알두기( 버튼번호 , 알모양 ){
+	// 1. <div> 버튼 목록 가져와서 배열에 저장 [ div 9개 호출 ]
+	let 버튼목록 = document.querySelectorAll('.버튼');	
+	// 2.배열에 상태 변경  // * 만약에 빈자리 가 아니면 실패 false 반환 
+	if( 버튼목록현황[버튼번호-1] !=  null ){ return false; }
+	// 3. 알모양과 상태 변경 // 클릭된 번호의 div 찾기 [ 선택번호-1 ] // -1 : 인덱스 0부터 시작하니까
+	버튼목록[버튼번호-1].innerHTML = 알모양 ; 버튼목록현황[버튼번호-1] = 알모양; 
+	// 4. 성공 true 반환
+	return true; // 성공했으면 true 반환
 }
