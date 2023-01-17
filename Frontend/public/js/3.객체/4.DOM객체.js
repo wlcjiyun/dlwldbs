c370db2a5d755b0222059bed88bba002a8cbe24f
504 <504@DESKTOP-DBOC81R>
2023-01-17 10:54:25 +0900

    .

--- Frontend/public/js/3.객체/4.DOM객체.js
+++ Frontend/public/js/3.객체/4.DOM객체.js
@@ -18,17 +18,26 @@ let h1 = `<h1>${ '안녕하세요' }</h1>`
 document.body.innerHTML = h1 
 
 // 2. DOM 객체의 이벤트
+function 함수1(){ console.log('문서열림') }
 	// 1. 기존 함수 적용
 	// document.addEventListener('이벤트명' , 함수) : 해당 문서에서 이벤트가 발생하면 함수 실행
-function 함수1(){ console.log('문서열림') }
-document.onload = 함수1
-document.addEventListener( 'load' , 함수1 )
+	// window.onload : HTML에서 모든 자료를 불러왔을 때 실행
+	// window.addEventListener( '이벤트명' , 함수 ) : 
+window.onload = 함수1
+	// DOMContentLoaded : HTML이 열렸을 때 다른 자료와 상관없이 무조건 실행됨
+document.addEventListener( 'DOMContentLoaded' , 함수1 )
+
 	// 2. 익명[이름없는] 함수 적용
-document.onload = function(){ console.log( '문서열림2' ) }
+window.onload = function(){ console.log( '문서열림2' ) }
 document.addEventListener( 'load' , function(){ console.log( '문서열림2' ) } )
 
+	// 3. 화살표 : (인수) => {실행문} : 함수 적용
+window.onload = () => { console.log( '문서열림3' ) }
+document.addEventListener( 'load' , () => { console.log( '문서열림3' ) } )
 
-
+let 익명함수 = function (){ console.log('문서열림5') }
+let 화살표함수 = ( ) => { console.log('문서열림4') }
+let 객체 = { 행동 : () => { console.log('문서열림6') }
 
 
 
