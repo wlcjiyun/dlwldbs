9fefac8891c53e40d7dab4e75767c5e0857f2056
504 <504@DESKTOP-DBOC81R>
2023-01-18 15:21:49 +0900

    .

--- Frontend/public/js/0.과제/과제5/과제5.js
+++ Frontend/public/js/0.과제/과제5/과제5.js
@@ -1,8 +1,19 @@
-/* 공통 */
+/* 공통 *///				0		  1		  2		   3		  4
 let categoryList = [ '프리미엄' , '스페셜' , '와퍼' , '올데이킹' , '치킨버거' ]
+
+let burgerList = [ 
+	{ name:'콰트로치즈X' , price:8200 , img:'버거킹버거1.png' , category:'프리미엄' } ,
+	{ name:'기네스와퍼' , price:7200 , img:'버거킹버거2.png' , category:'프리미엄' } ,
+	{ name:'몬스터와퍼' , price:9200 , img:'버거킹버거3.png' , category:'스페셜' } ,
+	{ name:'갈릭불고기와퍼' , price:6200 , img:'버거킹버거5.png' , category:'와퍼' } 
+]
+
+let cartList = [  ]
 
 category_print();
 categoty_select(0);
+product_ptint(0);
+
 // 1. 카테고리 출력 함수
 function category_print(){
 	
@@ -19,6 +30,7 @@ function category_print(){
 	document.querySelector('.categorybox').innerHTML = html
 }
 
+// 2. 카테고리 선택 함수
 function categoty_select( i ){ // i : 선택된 li의 인덱스
 
 	// 1. 모든 li를 가져와서 배열에 저장
@@ -32,7 +44,59 @@ function categoty_select( i ){ // i : 선택된 li의 인덱스
 			categotyli[j].classList.remove( 'categoryselect' ); // 해당 마크업의 class 식별자 제거
 		} // else e
 	} // for e
+	
+	// 3. 제품목록 렌더링
+	product_ptint( i )
 }
+
+// 3. 제품 출력 함수
+function product_ptint(index){
+	
+	// 1. HTML 구성
+	let html = ''; //깡통
+	
+	for( let i = 0 ; i<burgerList.length; i++ ){ // for S
+	// i는 0번째 인덱스부터 마지막 인덱스까지 버거 객체를 가져온다
+	
+		if( burgerList[i].category == categoryList[index] ){ // if S
+		// i번째 버거객체의 카테고리와 선택된 카테고리와 같으면
+		
+		html += `<div class="product">
+					<img src="img/${ burgerList[i].img }" width="100%"/>
+					<div class="productinfo">
+						<div class="ptitle"> ${ burgerList[i].name } </div>
+						<div class="pprice"> ${ burgerList[i].price.toLocaleString() } </div>
+					</div>
+				</div>`
+		} // if E
+	} // for E
+	
+	// 2. 구성된 HTML 마크업에 대입
+	document.querySelector('.productbox').innerHTML = html
+}
+
+// 4. 카트에 담기
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
