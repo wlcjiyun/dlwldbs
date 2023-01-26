1c8fae8b13780d80d9aa017b325d2e6da297e234
504 <504@DESKTOP-DBOC81R>
2023-01-26 17:47:06 +0900

    DAY17

--- jspw/src/main/webapp/js/index.js
+++ jspw/src/main/webapp/js/index.js
@@ -11,30 +11,28 @@ let productList = [
 product_print();
 
 function product_print(){
-	
 	let html = ``
-	productList.forEach( (o,i) => {
+	productList.forEach( ( o , i ) => { 
 		html += `
-				<div class="item">
-					<img alt="" src="img/${o.img}">
-					<div class="item_info">
-						<div class="item_title">${o.title}</div>
-						<div class="item_size">${o.size}</div>
-						<div class="item_price">${o.price.toLocaleLowerCase()}원</div>
-						<div>
-							<span class="item_sale">${(o.price - o.price*o.discount).toLocaleString()}원</span>
-							<span class="item_discount">${o.discount*100}</span>
-						</div>
-					</div>
-					<div class="item_bottom">
-						<div>
-							<span class="badge rounded-pill text-bg-warning">주문폭주</span>
-							<span class="badge rounded-pill text-bg-danger">인기상품</span>
-						</div>
-						<div class="item_review">찜 ${o.like} | 리뷰 ${o.rivew}</div>
+			<div class="item"> 					
+				<img src="img/${ o.img }">
+				<div class="item_info">
+					<div class="item_title">${ o.title }</div>	
+					<div class="item_size">${ o.size }</div>	
+					<div class="item_price">${ o.price.toLocaleString() } 원</div>
+					<div>
+						<span class="item_sale">${ ( o.price - (o.price*o.discount) ) .toLocaleString() } 원</span>
+						<span class="item_discount">${ o.discount*100 }%</span>
 					</div>
 				</div>
-				`
-	})		
+				<div class="item_bottom">
+					<div>
+						<span class="badge rounded-pill text-bg-warning">주문폭주</span>
+						<span class="badge rounded-pill text-bg-danger">1+1</span>
+					</div>
+					<div class="item_review"> 찜 ${ o.like } 리뷰수 ${ o.rivew }</div>	
+				</div>
+			</div>`
+	} )
 	document.querySelector('.itembox').innerHTML = html;
 }
