63422b6300c78123f73a0139386a1ca6d01f0261
504 <504@DESKTOP-DBOC81R>
2023-01-30 17:59:18 +0900

    .

--- jspw/src/main/webapp/css/board/e_enquiry.js
+++ jspw/src/main/webapp/css/board/e_enquiry.js
@@ -1,4 +1,14 @@
-
+function e_writer(){
+	
+	let board = {
+		writer : document.querySelector('.e_writer').value,
+		password : document.querySelector('.e_password').value,
+		title : document.querySelector('.e_title').value,
+		content : document.querySelector('.e_content').value
+	}
+	console.log(board)
+	
+}
 
 
 
