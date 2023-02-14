904a69e5c52004b4205a2970764a7ca52f5517ee
504 <504@DESKTOP-ENDCD90>
2023-02-14 14:34:35 +0900

    Day09.Ex3_p236

--- backed/src/Day09/Ex3_p236/실행.java
+++ backed/src/Day09/Ex3_p236/실행.java
@@ -2,20 +2,21 @@ package Day09.Ex3_p236;
 
 public class 실행 {
 	// 1. 객체의 필드를 초기화 하는 방법
-		// - 필드 / 직접접근
+		// 1. 필드 [ 직접 ] 
 		Car car = new Car();
 		car.gas = 5;
 	
-		// - 생성자 / 간접접근 : 객체의 필수 데이터
+		// 2. 생성자 [ 간접 ] : 객체의 필수 데이터
 		Car yourCar = new Car(5);
 	
-		// - 메소드 / 간접접근 : 객체 생성 후 데이터
-		Car myCar = new Car(); // 객체 생성
-		myCar.setGas(5); // 객체 내 함수 호출
+		// 3. 메소드 [ 간접 ] : 객체 생성후 데이터
+		Car myCar = new Car();	// 객체 생성 
+		myCar.setGas( 5 );		// 객체내 함수 호출 
 		
-		if( myCar.isLeftGas() ) { // 객체 내 함수 호출
-			System.out.println("출발합니다");
+		if( myCar.isLetfGas() ) {	// 객체내 함수 호출 
+			System.out.println("출발합니다.");
 			myCar.run();
 		}
+		
 		System.out.println("gas를 주입하세요");
 }
