ea9f18b1d6c70ae2b60a669aeac50d055c44c1e2
504 <504@DESKTOP-ENDCD90>
2023-02-16 14:29:57 +0900

    Day29

--- backed/src/Day09/Ex3_p236/Car.java
+++ backed/src/Day09/Ex3_p236/Car.java
@@ -7,34 +7,35 @@ package Day09.Ex3_p236;
 public class Car { // 클래스 [ JVM 메소드영역 로드]
 	
 	// 1. 필드
-		int gas;
-		// 2. 생성자
-		Car(){}
-		Car( int gas ){ this.gas = gas; }
-		// 3. 메소드
-			// 1.리턴타입X 인수O[ int형 매개변수 ] 
-		void setGas( int gas ) { // gas 필드에 데이터 저장하는 함수
-			this.gas = gas;
+	int gas;
+	
+	// 2. 생성자
+	Car(){}
+	Car( int gas ){ this.gas = gas; }
+	// 3. 메소드
+		// 1.리턴타입X 인수O[ int형 매개변수 ] 
+	void setGas( int gas ) { // gas 필드에 데이터 저장하는 함수
+		this.gas = gas;
+	}
+		// 2. 리턴타입O[boolean] 인수x
+	boolean isLetfGas() { // gas 여부 확인 함수
+		if( gas == 0 ) {
+			System.out.println("gas가 없습니다.");
+			return false;
 		}
-			// 2. 리턴타입O[boolean] 인수x
-		boolean isLetfGas() { // gas 여부 확인 함수
-			if( gas == 0 ) {
-				System.out.println("gas가 없습니다.");
-				return false;
+		System.out.println("gas가 있습니다.");
+		return true;
+	}
+		// 3. 리턴타입x 인수x
+	void run() {	// 자동차 달리[gas 0일때까지 ]는 함수 
+		while(true) { // 무한루프 [ 종료조건 : gas 0 이면 return ] 
+			if( gas > 0 ) {	// 만약에 gas가 0보다 크면 
+				System.out.println("달립니다.(gas잔량 "+gas+")");
+				gas--;	// gas 줄이기 
+			}else {
+				System.out.println("멈춥니다.(gas잔량 "+gas+")");
+				return; // 함수 종료[ while 종료 ]
 			}
-			System.out.println("gas가 있습니다.");
-			return true;
 		}
-			// 3. 리턴타입x 인수x
-		void run() {	// 자동차 달리[gas 0일때까지 ]는 함수 
-			while(true) { // 무한루프 [ 종료조건 : gas 0 이면 return ] 
-				if( gas > 0 ) {	// 만약에 gas가 0보다 크면 
-					System.out.println("달립니다.(gas잔량 "+gas+")");
-					gas--;	// gas 줄이기 
-				}else {
-					System.out.println("멈춥니다.(gas잔량 "+gas+")");
-					return; // 함수 종료[ while 종료 ]
-				}
-			}
-		}
+	}
 }
--- backed/src/Day09/Ex3_p236/실행.java
+++ backed/src/Day09/Ex3_p236/실행.java
@@ -1,22 +1,24 @@
 package Day09.Ex3_p236;
 
 public class 실행 {
-	// 1. 객체의 필드를 초기화 하는 방법
-		// 1. 필드 [ 직접 ] 
-		Car car = new Car();
-		car.gas = 5;
-	
-		// 2. 생성자 [ 간접 ] : 객체의 필수 데이터
-		Car yourCar = new Car(5);
-	
-		// 3. 메소드 [ 간접 ] : 객체 생성후 데이터
-		Car myCar = new Car();	// 객체 생성 
-		myCar.setGas( 5 );		// 객체내 함수 호출 
+	public static void main(String[] args) {
+		// 1. 객체의 필드를 초기화 하는 방법
+			// 1. 필드 [ 직접 ] 
+			Car car = new Car();
+			car.gas = 5;
 		
-		if( myCar.isLetfGas() ) {	// 객체내 함수 호출 
-			System.out.println("출발합니다.");
-			myCar.run();
-		}
+			// 2. 생성자 [ 간접 ] : 객체의 필수 데이터
+			Car yourCar = new Car(5);
 		
-		System.out.println("gas를 주입하세요");
+			// 3. 메소드 [ 간접 ] : 객체 생성후 데이터
+			Car myCar = new Car();	// 객체 생성 
+			myCar.setGas( 5 );		// 객체내 함수 호출 
+			
+			if( myCar.isLetfGas() ) {	// 객체내 함수 호출 
+				System.out.println("출발합니다.");
+				myCar.run();
+			}
+			
+			System.out.println("gas를 주입하세요");
+	}
 }
